4c805731b511e3a24f21360052e250cb9df27ffd
abs960 <a.b.s960@gmail.com>
2016-10-08 01:33:18 +0300

    [CGA-3] Can now easily stop using the section window by modifying the DRAWN_FIGURES_COUNT.

--- SDL_demo/draw.h
+++ SDL_demo/draw.h
@@ -4,9 +4,13 @@
 #include "stdafx.h"
 
 #define RAD(angle) ((angle) * M_PI / 180)
+#define RGB32(r, g, b) static_cast<uint32_t>((((static_cast<uint32_t>(r) << 8) | g) << 8) | b)
 
-const int	DRAWN_FIGURES_COUNT = 2, 
-			RADIUS = 100;
+const int	DRAWN_FIGURES_COUNT = 4, 
+			RADIUS = 100, 
+			HOW_MUCH_YOU_REALLY_WANT_TO_DRAW = 2;
+
+const bool	USE_SECTION_WINDOW = DRAWN_FIGURES_COUNT <= HOW_MUCH_YOU_REALLY_WANT_TO_DRAW;
 
 typedef float** Matrix;
 struct Point {
